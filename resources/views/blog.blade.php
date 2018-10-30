37566e1988bca2ab28d9576a791e6a40d95a0739
JOSE MATA CAMAMAHO <jguadalupemata@hotmail.com>
2018-10-30 09:02:07 -0600

    eliminar archivos
    eliminar archivos iniciales

--- resources/views/blog.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title','Blog')
-
-@section('content')
-<h1>blog</h1>
-@endsection
--- resources/views/name.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('title','Names')
-
-@section('content')
-<h1>Name</h1>
-@endsection
